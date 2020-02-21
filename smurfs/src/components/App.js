a2971d0be66d16aeefced506ffb5fe1429a4cbb7
gavin stahl <stahlgavin@gmail.com>
2020-02-21 08:44:07 -0800

    created initialState and reducer

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
-import SmurfForm from './SmurfForm';
-import SmurfList from './SmurfList';
+import SmurfForm from "./SmurfForm";
+import SmurfList from "./SmurfList";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Smurf's Village!</h1>
-        <div><SmurfForm /></div>
-        <div><SmurfList /></div>
+        <div>
+          <h2>Let's Add Some New Smurfs!</h2>
+          <SmurfForm />
+        </div>
+        <div>
+          <h2>Click To Update Smurf Village</h2>
+          <button>Update Village</button>
+          <SmurfList />
+        </div>
       </div>
     );
   }
